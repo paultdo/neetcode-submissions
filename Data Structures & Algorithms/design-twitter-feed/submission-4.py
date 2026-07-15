fbfe34b489d6d2514d8e3251ccbb32f272eae92d
paultdo <33135316+paultdo@users.noreply.github.com>
2026-07-15 15:46:56 -0700

    Add: design-twitter-feed - submission-4

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-4.py
@@ -0,0 +1,36 @@
+class Twitter:
+
+    def __init__(self):
+        self.count = 0
+        self.users = defaultdict(list) # user: [(count, tweet)]
+        self.follows = defaultdict(set) # user: [followees]
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.count += 1
+        self.users[userId].append((self.count, tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        heap = []
+        feed = []
+        for i in range(len(self.users[userId])):
+            heap.append(self.users[userId][i])
+        
+        for id in self.follows[userId]:
+            for tweet in self.users[id]:
+                heap.append(tweet)
+        
+        heapq.heapify_max(heap)
+
+        i = 0
+        while i < 10 and heap:
+            feed.append(heapq.heappop_max(heap)[1])
+            i += 1
+        return feed
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId and followeeId not in self.follows[followerId]:
+            self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId and followeeId in self.follows[followerId]:
+            self.follows[followerId].remove(followeeId)
